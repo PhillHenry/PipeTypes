abb3e956f8685f48f7994ba20a0d6e805e0f5d69
Phillip Henry <PhillHenry@gmail.com>
2022-01-14 12:16:07 +0000

    Refactor.

--- src/main/scala/uk/co/odinconsultants/pipetypes/data/RefinedPatient.scala
+++ src/main/scala/uk/co/odinconsultants/pipetypes/data/RefinedPatient.scala
@@ -9,15 +9,15 @@ import numeric.constraint.{given, *}
 
 object TestTypes {
 
-  type Aged = Int  / (Greater[-1] && Less[120])
-  type Binaryd = Int / (-1 < ?? < 2)
+  type Age = Int  / (Greater[-1] && Less[120])
+  type Binary = Int / (-1 < ?? < 2)
 
-  def accepted(age: Aged) = {
+  def accepted(age: Age) = {
     val binary = age.flatMap { age =>
-      val binary: Binaryd = if (age < 40) {
-        1: Binaryd
+      val binary: Binary = if (age < 40) {
+        1: Binary
       } else {
-        0: Binaryd
+        0: Binary
       }
       binary
     }
@@ -28,6 +28,7 @@ object TestTypes {
   def main(args: Array[String]): Unit = {
     val oldFart = accepted(42)
 //    val doesNotCompile = accepted(121)
+//    val doesNotCompile = accepted(0: Binaryd)
   }
 
 }
