90802edc563ff81583b3e47af9720db29576757e
Phillip Henry <PhillHenry@gmail.com>
2022-01-14 12:05:11 +0000

    Well, at least it compiles

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,9 @@ object Dependencies {
 	lazy val cats  = "org.typelevel" %% "cats-core" % "2.6.1"
 	lazy val refined = "eu.timepit" %% "refined" % "0.9.28"
 	lazy val munit = "org.scalameta" %% "munit" % "0.7.26" % Test
+	lazy val iron = "io.github.iltotore" %% "iron" % "1.2.0"
+	lazy val iron_numeric = "io.github.iltotore" %% "iron-numeric" % "1.2-1.0.0"
 
-	lazy val compileDeps = Seq(cats, refined)
+	lazy val compileDeps = Seq(cats, refined, iron, iron_numeric)
 	lazy val testDeps    = Seq(munit)
 }
