dc95062137fc12774e551fcc5c7add391a6f83e4
Phillip Henry <PhillHenry@gmail.com>
2022-01-15 08:45:08 +0000

    Refine to a binary.

--- src/main/scala/uk/co/odinconsultants/pipetypes/data/RefinedPatient.scala
+++ src/main/scala/uk/co/odinconsultants/pipetypes/data/RefinedPatient.scala
@@ -2,17 +2,19 @@ package uk.co.odinconsultants.pipetypes.data
 
 import scala.math.Ordered.orderingToOrdered
 import scala.math.Ordering.Implicits.infixOrderingOps
-
+import io.github.iltotore.iron.constraint.refineValue
 import io.github.iltotore.iron.*, constraint.{given, *}
 import numeric.constraint.{given, *}
 
 
 object TestTypes {
 
-  type Age = Int  / (Greater[-1] && Less[120])
+  type Age = Int  / ((0 <= ?? <= 120) DescribedAs "Reasonable ages should be between 0 and 120")
   type Binary = Int / (-1 < ?? < 2)
 
-  def accepted(age: Age) = {
+  def accepted(age: Age): Refined[Int] = {
+    println(s"age type = ${age.getClass.getName}")
+    println(age.getClass.getTypeParameters.map(_.getBounds.map(_.getTypeName).mkString(", ")).mkString(", "))
     val binary = age.flatMap { age =>
       val binary: Binary = if (age < 40) {
         1: Binary
@@ -25,10 +27,17 @@ object TestTypes {
     binary
   }
 
+  def accept(x: Binary): Binary = {
+    print(s"accept $x")
+    x
+  }
+
   def main(args: Array[String]): Unit = {
     val oldFart = accepted(42)
+    oldFart.flatMap(x => accept(refineValue(x)))
+//    oldFart.map(accept)
 //    val doesNotCompile = accepted(121)
-//    val doesNotCompile = accepted(0: Binaryd)
+//    val doesNotCompile = accepted(0: Binary)
   }
 
 }
