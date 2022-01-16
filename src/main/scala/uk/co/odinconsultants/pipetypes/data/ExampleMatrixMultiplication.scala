2423beb9d9b0b25a2fa1c21334315e2e3d316d82
Phillip Henry <PhillHenry@gmail.com>
2022-01-16 11:20:54 +0000

    Matrix example.

---
+++ src/main/scala/uk/co/odinconsultants/pipetypes/data/ExampleMatrixMultiplication.scala
@@ -0,0 +1,29 @@
+package uk.co.odinconsultants.pipetypes.data
+
+type Dimension = Int & Singleton
+
+case class Matrix[T <: Int & Singleton, U <: Int & Singleton](data: List[Double])
+
+trait MatrixMultiplication {
+  def multiply[T <: Dimension, U <: Dimension, V <: Dimension]
+    (x: Matrix[T, U], y: Matrix[U, V]): Matrix[T, V]
+}
+
+object ExampleMatrixMultiplication {
+
+  def main(array: Array[String]): Unit = matrixMultiplicationExample(new MatrixMultiplication {
+    override def multiply[T <: Dimension, U <: Dimension, V <: Dimension](x: Matrix[T, U],
+                                                                          y: Matrix[U, V])
+    : Matrix[T, V] = ???
+  })
+
+  def matrixMultiplicationExample(ops: MatrixMultiplication) = {
+    val _3x7 = Matrix[3, 7](List())
+    val _7x4 = Matrix[7, 4](List())
+    val _3x5 = Matrix[3, 5](List())
+
+    ops.multiply(_3x7, _7x4)
+//    ops.multiply(_3x7, _3x5) // doesn't compile as expected
+  }
+
+}
