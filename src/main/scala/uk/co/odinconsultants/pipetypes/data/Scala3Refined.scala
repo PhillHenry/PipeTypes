16c3322350c0127012c2923c5f53f287f72ae45c
Phillip Henry <PhillHenry@gmail.com>
2022-02-05 10:01:00 +0000

    Using Michel Sitko's code.

---
+++ src/main/scala/uk/co/odinconsultants/pipetypes/data/Scala3Refined.scala
@@ -0,0 +1,52 @@
+package uk.co.odinconsultants.pipetypes.data
+
+import scala.compiletime.{erasedValue, constValue, error}
+import scala.compiletime.ops.int.ToString
+
+sealed trait Pred
+class And[A <: Pred, B <: Pred] extends Pred
+class Leaf extends Pred
+class LowerThan[T <: Int & Singleton] extends Leaf
+class GreaterThan[T <: Int & Singleton] extends Leaf
+
+trait Validated[E <: Pred]
+
+/**
+ * Shamelessly stolen from Michel Sitko's excellent blog post her:
+ * https://msitko.pl/blog/build-your-own-refinement-types-in-scala3.html
+ */
+implicit inline def mkValidated[V <: Int & Singleton, E <: Pred](v: V): Validated[E] =
+  inline erasedValue[E] match
+    case _: LowerThan[t] =>
+      inline if constValue[V] < constValue[t]
+        then new Validated[E] {}
+        else
+          inline val vs = constValue[ToString[V]]
+          inline val limit = constValue[ToString[t]]
+          error("Validation failed: " + vs + " < " + limit)
+    case _: GreaterThan[t] =>
+      inline if constValue[V] > constValue[t]
+        then new Validated[E] {}
+        else
+          inline val vs = constValue[ToString[V]]
+          inline val limit = constValue[ToString[t]]
+          error("Validation failed: " + vs + " > " + limit)
+    case _: And[a, b] =>
+      inline mkValidated[V, a](v) match
+        case _: Validated[_] =>
+          inline mkValidated[V, b](v) match
+            case _: Validated[_] => new Validated[E] {}
+
+
+object Demo:
+  val a: Validated[LowerThan[10]] = 6
+  
+  // val x: Validated[LowerThan[10]] = 16 // Validation fails with:
+  // Validation failed: 16 < 10
+
+  val b: Validated[GreaterThan[5] And LowerThan[10]] = 6
+  // val y: Validated[GreaterThan[5] And LowerThan[10]] = 1 // Validation fails with:
+  // Validation failed: 1 > 5
+  // val z: Validated[GreaterThan[5] And LowerThan[10]] = 16 // Validation fails with:
+  // Validation failed: 16 < 10
+
