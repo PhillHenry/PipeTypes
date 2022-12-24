afdcfe5d663e2e1fe4e851916c627e21dec0d6ac
PhillipHenry <PhillHenry@gmail.com>
2022-12-24 17:53:13 +0000

    Hmm, needs an explicit conversion to make it work. But it's progress.

--- src/main/scala/uk/co/odinconsultants/pipetypes/data/matrix/ExampleMatrixMultiplicationScala3.scala
+++ src/main/scala/uk/co/odinconsultants/pipetypes/data/matrix/ExampleMatrixMultiplicationScala3.scala
@@ -3,6 +3,7 @@ package uk.co.odinconsultants.pipetypes.data.matrix
 import io.github.iltotore.iron.*
 import io.github.iltotore.iron.constraint.all.{DescribedAs, *}
 import uk.co.odinconsultants.pipetypes.data.Matrix
+import scala.compiletime.{erasedValue, constValue, error}
 
 type DimensionType = Int :| DescribedAs[
   Greater[0],
@@ -15,9 +16,21 @@ class Matrix[X <: Dim, Y <: Dim]
 
 object ExampleMatrixMultiplicationScala3 {
 
+  class ValidShape[X <: Dim, Y <: Dim]()
+
+  transparent implicit inline def validate[X <: Dim, Y <: Dim](m: Matrix[X, Y]): ValidShape[X, Y] = {
+    inline erasedValue[X] match
+      case x: Dim if x <= 0 => error("boom!")
+      case _ => new ValidShape[X, Y]
+  }
+
+  def itWorks[X <: Dim, Y <: Dim](x: ValidShape[X, Y]): Unit = println(x)
+
   def main(args: Array[String]): Unit = {
     println("hello, world")
-    new Matrix[3, 2]
+    itWorks(new Matrix[3, 2])
+//    val x = new Matrix[-3, -2]
+//    itWorks(x)
   }
 
 }
