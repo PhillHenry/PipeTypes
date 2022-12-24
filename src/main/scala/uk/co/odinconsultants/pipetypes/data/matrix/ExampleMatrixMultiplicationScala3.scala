07667e04a7d5d99a717ca72bf60a9694d2b2dffd
PhillipHenry <PhillHenry@gmail.com>
2022-12-24 13:01:14 +0000

    Int & Singleton works as a type but not the refined type....

--- src/main/scala/uk/co/odinconsultants/pipetypes/data/matrix/ExampleMatrixMultiplicationScala3.scala
+++ src/main/scala/uk/co/odinconsultants/pipetypes/data/matrix/ExampleMatrixMultiplicationScala3.scala
@@ -4,18 +4,18 @@ import io.github.iltotore.iron.*
 import io.github.iltotore.iron.constraint.all.{DescribedAs, *}
 import uk.co.odinconsultants.pipetypes.data.Matrix
 
-type Dimension = Int :| DescribedAs[
+type DimensionType = Int :| DescribedAs[
   Greater[0],
   "Dimensions of a matrix must be > 0"
 ]
 
-class Matrix[X <: Dimension, Y <: Dimension](x: X, y: Y)
+class Matrix[X <: Int & Singleton, Y <: Int & Singleton]
 
 object ExampleMatrixMultiplicationScala3 {
 
   def main(args: Array[String]): Unit = {
     println("hello, world")
-    val _3x2 = new Matrix(3, 2)
+    new Matrix[3, 2]
   }
 
 }
