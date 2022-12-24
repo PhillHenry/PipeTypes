01ccda15403a3d6b49412fcf6fc2cc6cc7d22996
PhillipHenry <PhillHenry@gmail.com>
2022-12-24 10:30:40 +0000

    Refactor.

--- src/main/scala/uk/co/odinconsultants/pipetypes/data/RefinedPatient.scala
+++ src/main/scala/uk/co/odinconsultants/pipetypes/data/RefinedPatient.scala
@@ -8,24 +8,24 @@ import io.github.iltotore.iron.constraint.all.*
 
 object ExamplePipeline {
 
-  type Age = DescribedAs[
+  type Age = Int :| DescribedAs[
     Greater[0] & Less[121],
     "Reasonable ages should be between 0 and 120"
   ]
-  type BinaryInteger = DescribedAs[
+  type BinaryInteger = Int :| DescribedAs[
     StrictEqual[0] | StrictEqual[1],
     "Binary integer values are either 1 or 0"
   ]
 
-  def ageToBinary(age: Int :| Age): Int :| BinaryInteger =
-      val binary: Int :| BinaryInteger = if (age < 40) {
+  def ageToBinary(age: Age): BinaryInteger =
+      val binary: BinaryInteger = if (age < 40) {
         1
       } else {
         0
       }
       binary
 
-  def accept(x: Int :| BinaryInteger): Int :| BinaryInteger = {
+  def accept(x: BinaryInteger): BinaryInteger = {
     println(s"accept $x")
     x
   }
