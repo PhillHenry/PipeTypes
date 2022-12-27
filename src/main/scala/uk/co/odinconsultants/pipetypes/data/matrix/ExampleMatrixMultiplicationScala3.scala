86e4a5b1b22514618dfb4fab6934403a450755a3
PhillipHenry <PhillHenry@gmail.com>
2022-12-27 16:33:06 +0000

    Lining ducks in a row.

--- src/main/scala/uk/co/odinconsultants/pipetypes/data/matrix/ExampleMatrixMultiplicationScala3.scala
+++ src/main/scala/uk/co/odinconsultants/pipetypes/data/matrix/ExampleMatrixMultiplicationScala3.scala
@@ -11,11 +11,12 @@ type DimensionType = Int :| DescribedAs[
 
 type Dim = Int & Singleton
 
-class Matrix[X <: Dim, Y <: Dim]
+
 
 object MatrixOps {
   class ValidMatrix[X <: Dim, Y <: Dim]()
   opaque type Validated[X <: Dim, Y <: Dim] = ValidMatrix[X, Y]
+
 
   transparent implicit inline def validate[X <: Dim, Y <: Dim](m: Matrix[X, Y]): Validated[X, Y]
   = {
@@ -23,8 +24,12 @@ object MatrixOps {
       case x: Dim if x <= 0 => error("boom!")
       case _ => new ValidMatrix[X, Y]
   }
-
-  def mustBeValid[X <: Dim, Y <: Dim](x: Validated[X, Y]): Unit = println(x)
+  class ValidD[Dim]
+  class Matrix[X <: Dim, Y <: Dim]
+  transparent implicit inline def toValid[X <: Dim](m: X): ValidD[X] = {
+    inline if (m <= 0) error("boom!") else new ValidD[X]
+  }
+  def mustBeValid(x: ValidD[Dim]): Unit = println(x)
 }
 
 object ExampleMatrixMultiplicationScala3 {
@@ -32,11 +37,14 @@ object ExampleMatrixMultiplicationScala3 {
 
   def main(args: Array[String]): Unit = {
     println("hello, world")
-    val invalid = new Matrix[-3, -2]
-    val valid = new Matrix[3, 2]
     import MatrixOps.*
     import MatrixOps.validate
-    mustBeValid(valid)
+    import MatrixOps.toValid
+    //    val invalid = new Matrix[-3, -2]
+    val valid = new Matrix[3, 2]
+    mustBeValid(1)
+//    mustBeValid(-1) // boom!
+//    mustBeValid(valid)
 //    mustBeValid(invalid) // boom!
   }
 
