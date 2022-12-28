08f5a3445f2057fe56ec79c6ec5a1f69b608c04c
PhillipHenry <PhillHenry@gmail.com>
2022-12-28 10:57:55 +0000

    Assertion.

--- src/test/scala/uk/co/odinconsultants/pipetypes/data/matrix/MatrixSpec.scala
+++ src/test/scala/uk/co/odinconsultants/pipetypes/data/matrix/MatrixSpec.scala
@@ -6,6 +6,7 @@ import scala.compiletime.testing.typeCheckErrors
 class MatrixSpec extends munit.FunSuite:
   test("Those should not compile") {
     assertEquals(typeCheckErrors("Matrix[1, -2]").map(_.message), List(ValidDimensionMsg))
+    assertEquals(typeCheckErrors("Matrix[-1, 2]").map(_.message), List(ValidDimensionMsg))
     assertEquals(typeCheckErrors("invert(Matrix[1, 2])").size, 1)
   }
   test("These shoule compile") {
