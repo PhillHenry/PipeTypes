d23062bf6afbcbbf929699497f3a697c3ed02825
PhillipHenry <PhillHenry@gmail.com>
2022-12-28 10:45:32 +0000

    More sensible error.

--- src/main/scala/uk/co/odinconsultants/pipetypes/data/matrix/Matrix.scala
+++ src/main/scala/uk/co/odinconsultants/pipetypes/data/matrix/Matrix.scala
@@ -5,13 +5,14 @@ type D = Int & Singleton
 class Matrix[X <: D, Y <: D]
 
 object Matrix {
+  inline val ValidDimensionMsg = "Matrix dimensions must be > 0"
 
   opaque type M[X <: D, Y <: D] = Matrix[X, Y]
   transparent inline def apply[X <: D, Y <: D]: M[X, Y] = inline erasedValue[X] match
-    case x: D if x <= 0 => error("boom!")
+    case x: D if x <= 0 => error(ValidDimensionMsg)
     case _              =>
       inline erasedValue[Y] match
-        case y: D if y <= 0 => error("boom!")
+        case y: D if y <= 0 => error(ValidDimensionMsg)
         case _              => new Matrix[X, Y]
 
   def main(args: Array[String]): Unit = {
--- src/test/scala/uk/co/odinconsultants/pipetypes/data/matrix/MatrixSpec.scala
+++ src/test/scala/uk/co/odinconsultants/pipetypes/data/matrix/MatrixSpec.scala
@@ -1,7 +1,9 @@
 package uk.co.odinconsultants.pipetypes.data.matrix
+import uk.co.odinconsultants.pipetypes.data.matrix.Matrix.*
+
 import scala.compiletime.testing.typeCheckErrors
 class MatrixSpec extends munit.FunSuite:
     test("Those should not compile") {
     assertEquals(typeCheckErrors("Matrix[1, -2]").map(_.message),
-      List("boom!"))
+      List(ValidDimensionMsg))
   }
