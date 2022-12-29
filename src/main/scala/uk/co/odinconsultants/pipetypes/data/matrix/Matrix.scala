e2ff09bdfab8cd7cbe9d84bb1f32c9cf7f2b57f4
PhillipHenry <PhillHenry@gmail.com>
2022-12-29 11:35:52 +0000

    Not sure why we need literals again but at least it compiles like this.

--- src/main/scala/uk/co/odinconsultants/pipetypes/data/matrix/Matrix.scala
+++ src/main/scala/uk/co/odinconsultants/pipetypes/data/matrix/Matrix.scala
@@ -3,30 +3,39 @@ import scala.compiletime.{erasedValue, constValue, error}
 //import io.github.iltotore.iron.*, constraint.*, iterable.constraint.*
 
 type D = Int & Singleton
-case class Matrix[X <: D, Y <: D](xs: List[Double])
+class Matrix[X <: D, Y <: D]
 
 object Matrix {
   inline val InvalidDimensionMsg    = "Matrix dimensions must be > 0"
   inline val InvalidCoefficientsMsg = "Number of coefficients incompatible with dimensions"
 
-  opaque type M[X <: D, Y <: D] = Matrix[X, Y]
+  type M[X <: D, Y <: D] = Matrix[X, Y]
 
   transparent inline def apply[X <: D, Y <: D](inline xs: List[Double]): M[X, Y] =
     import scala.compiletime.*
     import scala.compiletime.ops.int.*
     inline erasedValue[X] match
-      case x: D if x <= 0 => error(InvalidDimensionMsg)
+      case x: D if x <= 0 => error("Matrix dimensions must be > 0")
       case x              =>
         inline erasedValue[Y] match
+          case y: D if y <= 0                     => error("Matrix dimensions must be > 0")
           // "inline match can only work on the static type of a value. List("a", "b") has the
           // type List[String] which can not be further inspected"
           // https://github.com/lampepfl/dotty/issues/14917
-//          case y: D if (x * y) == xs.size => error(InvalidCoefficientsMsg) // "Cannot reduce inline match..."
-          case y: D if y <= 0             => error(InvalidDimensionMsg)
-          case _                          => new Matrix[X, Y](xs)
+          case y: D if (x * y) != checkLength(xs) =>
+            error(
+              "Number of coefficients incompatible with dimensions"
+            ) // "Cannot reduce inline match..."
+          case _                                  => new Matrix[X, Y]
+  transparent inline def checkLength(inline xs: List[Double]): Int               =
+    ${ checkLengthC('xs) }
+  import scala.quoted._
+  def checkLengthC(v: Expr[List[Double]])(using Quotes): Expr[Int] =
+//    val res = v.valueOrError.startsWith(pred.valueOrError)
+    Expr(v.valueOrAbort.size)
 
   transparent inline def invert[X <: D](m: M[X, X]): M[X, X]            = m // TODO the actual inversion
   transparent inline def transpose[X <: D, Y <: D](m: M[X, Y]): M[Y, X] =
-    new Matrix[Y, X](m.xs) // TODO the actual transpose
+    new Matrix[Y, X] // TODO the actual transpose
 
 }
--- src/test/scala/uk/co/odinconsultants/pipetypes/data/matrix/MatrixSpec.scala
+++ src/test/scala/uk/co/odinconsultants/pipetypes/data/matrix/MatrixSpec.scala
@@ -6,7 +6,7 @@ import scala.compiletime.testing.typeCheckErrors
 class MatrixSpec extends munit.FunSuite:
   val xs = List(1d, 2d, 3d, 4d)
   test("Those should not compile") {
-//    assertEquals(typeCheckErrors("invert(Matrix[2, 2](List(1d, 2d)))").map(_.message), List(InvalidCoefficientsMsg))
+    assertEquals(typeCheckErrors("invert(Matrix[2, 2](List(1d, 2d)))").map(_.message), List(InvalidCoefficientsMsg))
     assertEquals(typeCheckErrors("Matrix[1, -2](List(1d, 2d))").map(_.message), List(InvalidDimensionMsg))
     assertEquals(typeCheckErrors("Matrix[-1, 2](List(1d, 2d))").map(_.message), List(InvalidDimensionMsg))
     assertEquals(typeCheckErrors("invert(Matrix[1, 2](List(1d, 2d)))").size, 1)
